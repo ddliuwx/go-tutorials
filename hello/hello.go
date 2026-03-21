9b07e5e9f51c92f5454e3c43eaf1c82b032c1741
“DongLiu” <dl398@students.waikato.ac.nz>
2026-03-21 21:36:47 +1300

    feat: add call greetings module function.

--- hello/hello.go
+++ hello/hello.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"example.com/greetings"
 	"fmt"
 	"rsc.io/quote"
 )
 
 func main() {
+	greetings.Hello("Gladys")
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
 }
