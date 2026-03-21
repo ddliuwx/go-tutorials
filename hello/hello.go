4d413c6fb6932ff452e8250fdf393c571dd0f872
“DongLiu” <dl398@students.waikato.ac.nz>
2026-03-21 22:29:17 +1300

    feat: add hellos function logic.

--- hello/hello.go
+++ hello/hello.go
@@ -12,7 +12,8 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Winter")
+	name := []string{"Kim", "Winter", "RR"}
+	message, err := greetings.Hellos(name)
 	if err != nil {
 		log.Fatal(err)
 	}
